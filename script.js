df52dad982ad9a9d1ea2f779c030d2d40aa01984
Mandus Lindström <mandus85@hotmail.com>
2025-09-22 14:44:39 +0200

    Done with code along

---
+++ script.js
@@ -0,0 +1,63 @@
+// TODO – En första funktion
+// console.log(addition(1, 5));
+// Funktion som adderar två tal
+function addition(x, y) {
+  //   console.log(1 + 2);
+  return x + y;
+}
+
+// const result = addition(21, 5);
+
+// console.log(result);
+
+// TODO – Function expression
+// Function expression
+
+const multiplyTwoNum = function (number1, number2) {
+  return number1 * number2;
+};
+//
+// const result = multiplyTwoNum(6, 6);
+// console.log(result);
+
+// TODO – Arrow function
+// Arrow function
+const subtract = (num1, num2) => {
+  return num1 - num2;
+};
+
+// const subtract = (num1, num2) => num1 - num2;
+
+// const hej = (arg) => arg;
+// const hej = (arg) => {
+//   return arg;
+// };
+
+// console.log(subtract(55, 5));
+// console.log(hej("halllåååå eller"));
+
+// TODO – Funktion med parameter + utskrift
+// Funktion som hälsar
+
+// console.log(result);
+
+// TODO – Loop: for
+// Array
+const cats = ["Milla", "chatti", "Simba"];
+
+// for (let i = 0; i < cats.length; i++) {
+//   console.log("Cats loopas igenom med en for loop", cats[i]);
+// }
+
+// TODO – Loop: for…of
+// for (const cat of cats) {
+//   console.log("Namn på min katt: ", cat);
+// }
+
+// TODO – Loop: while
+let counter = 0;
+
+while (counter < 5) {
+  console.log("While loopens varv är nu på:", counter);
+  counter++;
+}
